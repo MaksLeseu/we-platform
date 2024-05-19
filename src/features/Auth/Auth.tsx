1f31c45f544cbcc33acd9e0c11f42836ed6b4233
Maxim Lesev <lesov0iblr@gmail.com>
2024-05-19 21:24:57 +0200

    Add react-hook form and validate forms

--- src/features/Auth/Auth.tsx
+++ src/features/Auth/Auth.tsx
@@ -1,9 +1,44 @@
+import { useForm } from 'react-hook-form'
+
+type FromValue = {
+   loginOrEmail: string
+   password: string
+}
+
 export const Auth = () => {
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+   } = useForm<FromValue>()
+   console.log('errors: ', errors)
+
+   const onSubmit = (data: FromValue) => {
+      console.log(data)
+   }
+
+   const emailRegex =
+      /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
+
    return (
-      <div>
-         <input placeholder={'Email or Username'} />
-         <input placeholder={'Password'} />
-         <button>Sign In</button>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+         {errors.loginOrEmail?.message && <span>{errors.loginOrEmail.message}</span>}
+         <input
+            {...register('loginOrEmail', {
+               required: 'Email is required',
+               pattern: { value: emailRegex, message: 'Invalid email' },
+            })}
+            placeholder={'Email or Username'}
+         />
+         {errors.password?.message && <span>{errors.password.message}</span>}
+         <input
+            {...register('password', {
+               required: 'Password is required',
+               minLength: { value: 3, message: 'Password has to be at least 3 characters long' },
+            })}
+            placeholder={'Password'}
+         />
+         <button type={'submit'}>Sign In</button>
+      </form>
    )
 }
